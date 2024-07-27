82f27b3ab6fd20d91f5efa4cd8218d7be9755297
abdlghany <abdalghany@windowslive.com>
2024-07-27 16:08:58 +0800

    complex bmi calculator done

--- Stage2/Javascript/js/complexBmiCalc.js
+++ Stage2/Javascript/js/complexBmiCalc.js
@@ -1,80 +1,97 @@
-//setting default units to cm and kg.
 var selectedHeightUnit = "cm";
 var selectedWeightUnit = "kg";
-//setting default value to nothing
 var message = "";
-//devault value is 1 for BMI calculations.
 var decimal = 1;
+
 function calculateBMI(){
-    //make sure the changeUnits function works even if the user didn't change the unit but changed something else or clicked the button.
-    selectedHeightUnit = changeUnits(getValueOfElement("heightUnit"), "heightLabel");
-    selectedWeightUnit = changeUnits(getValueOfElement("weightUnit"), "weightLabel");
-    //trimming the name to get rid of spaces at the start and at the end.
+    selectedHeightUnit = changeUnits(getValueOfElement("heightUnit"), "heightLabel", "height");
+    selectedWeightUnit = changeUnits(getValueOfElement("weightUnit"), "weightLabel", "weight");
+    decimalChanged();
+    resetMessages();
+    //variable declaration
     var name = getValueOfElement("name").trim();
-    //multiplying by 1 to make sure the value gets converted to a number (can use parseInt instead).
     var weight = getValueOfElement("weight")*1;
     var height = getValueOfElement("height")*1;
-    //assign value to 0
     var bmi = 0;
-    //check if variable name isn't null or empty
-    if(name){
-        if(validateName(name)){
-            message = "Please insert a valid name!<br>Your name cannot contain numbers or symbols<br>or be less than 2 characters or more than 100.";
-        }
-        //check if weight variable isn't null or empty
-        else if(!weight){
-           message = "Weight field is required!<br>Cannot be 0";
-        }
-        else{
-             //check if the weight is valid, if not, give error message
-            if (!isValueBetween(weight,151,0)){
-              message = "Please insert a valid weight!<br>between 1kg and 150kg.";
+    var messageField = "";
+    
+    if (name) {
+        if (validateName(name)) {
+            messageField = "nameMessage";
+            message = "Please insert a valid name!<br>Only letters and spaces are allowed<br>Must be less than 2 characters or more than 100.";
+        } else if (!weight) {
+            messageField = "weightMessage";
+            message = "Weight field is required!<br>Cannot be 0";
+        } else {
+            if (selectedWeightUnit == "pound") {
+                    // Convert to kg
+                    weight = weight / 2.2046226;
             }
-            else if(!height){
-              message = "Height field is required!<br>Cannot be 0";
+            // Check if the weight is valid (in kg)
+            if (!isValueBetween(weight, 0.9, 150.1)) {
+                if(selectedWeightUnit == "pound"){
+                    messageField = "weightMessage";
+                    message = "Please insert a valid weight!<br>Between 2 pounds and 332 pounds."; 
+                }
+                else{
+                    messageField = "weightMessage";
+                    message = "Please insert a valid weight!<br>Between 1kg and 150kg.";    
+                    }
             }
-            else {
-                    if (isValueBetween(decimal, -1, 10)) {
-                        if (selectedHeightUnit == "inch") {     
-                            // Check if the height is valid in inches
-                            if (!isValueBetween(height, 15, 101)) {
-                                message = "Please insert a valid height!<br>Between 16 Inches and 100 Inches." + height;
-                            } else {
-                                // Converting height from inch to m
-                                height = height / 100 * 2.54;
-                                bmi = decimalRounder(weight / Math.pow(height, 2), decimal);
-                                message = bmiClassification(name, bmi, "kg", "m");
-                            }
+             else if (!height) {
+                messageField = "heightMessage";
+                message = "Height field is required!<br>Cannot be 0";
+            } else {
+                if (isValueBetween(decimal, -1, 10)) {
+                    if (selectedHeightUnit == "inch") {
+                        // Check if the height is valid in inches
+                        if (!isValueBetween(height, 15.9, 100.1)) {
+                            messageField = "heightMessage";
+                            message = "Please insert a valid height!<br>Between 16 Inches and 100 Inches.";
                         } else {
-                            // Check if the height is valid in cm
-                            if (!isValueBetween(height, 251, 39)) {
-                                message = "Please insert a valid height!<br>Between 40cm and 250cm." + + height;
-                            } else {
-                                // Converting height from cm to m
-                                height = height / 100;
-                                bmi = decimalRounder(weight / Math.pow(height, 2), decimal);
-                                message = bmiClassification(name, bmi, "kg", "m");
-                            }
+                            // Convert height from inches to meters
+                            height = height * 2.54 / 100;
+                            bmi = formatNumberWithDecimals(decimalRounder(weight / Math.pow(height, 2), decimal),decimal);
+                            message = bmiClassification(name, bmi, "kg", "m");
+                            messageField = "messageP";
                         }
                     } else {
-                        message = "Please insert a valid decimal.<br>Between 0 and 9";
+                        // Check if the height is valid in cm
+                        if (!isValueBetween(height, 39.9, 250.1)) {
+                            messageField = "heightMessage";
+                            message = "Please insert a valid height!<br>Between 40cm and 250cm.";
+                        } else {
+                            // Convert height from cm to meters
+                            height = height / 100;
+                            bmi = formatNumberWithDecimals(decimalRounder(weight / Math.pow(height, 2), decimal),decimal);
+                            message = bmiClassification(name, bmi, "kg", "m");
+                            messageField = "messageP";
+                        }
                     }
-            }            
+                } else {
+                    messageField = "decimalMessage";
+                    message = "Please insert a valid decimal.<br>Between 0 and 9";
+                }
             }
+        }
+    } else {
+        messageField = "nameMessage";
+        message = "Name field is required!";
     }
-    else{
-        message = "Name field is required!"
-    }
-    passMessageToElement("messageP", message);
+    passMessageToElement(messageField, message);
+    toggleMessagefield(messageField);
     return false;
 }
 
 function decimalChanged(){
     decimal = getValueOfElement("decimal")*1;
-    if(decimal){
-        //do nothing becuase the user has changed the default value.
-    }else{
-        //default value
-        decimal = 1;
+    if (decimal && decimal != 0) {
+        // Do nothing because the user has changed the default value and it's not invalid or null or 0.
+    } else if (decimal == 0) {
+        // Do nothing.
+    } else {
+        // Default to 1 if nothing exists instead of giving the user an error.
+        decimal = 1;      
     }
+
 }
--- Stage2/Javascript/js/myReusableFunctions.js
+++ Stage2/Javascript/js/myReusableFunctions.js
@@ -6,13 +6,11 @@ function decimalRounder(value, decimals){
     // = 5, then we divide by 10, we get 0.5
     return Math.round(value * powerOf) / powerOf;
 }
-
 /* function to change an element's inner HTML */
 function passMessageToElement (elementID, message){
     document.getElementById(elementID).innerHTML = message;
     return null
 }
-
 /* function to get the value of an elemeny by ID */
 function getValueOfElement(ElementID){
     elementValue = document.getElementById(ElementID).value;
@@ -23,33 +21,25 @@ function switchTheme(){
     document.getElementById("mainBody").classList.toggle("darkTheme");
 }
 //function that checks if the value is between 2 numbers
-function isValueBetween(value, x ,y){
-    if (value > x && value < y){
-        return true;
-    }
-    else if (value < x && value > y){
-        return true;
-    }
-    else {
-        return false;
-    }
+function isValueBetween(value, x, y) {
+    return (value > Math.min(x, y) && value < Math.max(x, y));
 }
 //function that returns a message with the bmi classification
 function bmiClassification(name, bmi, weightUnit, heightUnit){
     var classification = "";
     if(bmi < 18.5){
-        classification = "underweight";
+        classification = "red\">underweight";
     }
     else if(bmi < 25){
-        classification = "normal";
+        classification = "green\">normal</span>";
     }
     else if(bmi < 30){
-        classification = "overweight";
+        classification =  "orange\">overweight</span>";
     }
     else{
-        classification = "obese";
+        classification = "red\">obese</span>";
     }
-    return "Dear "+name+", Your BMI is: <b>"+ bmi +weightUnit+"/"+heightUnit+"<sup>2</sup></b> " + "<br/>Your BMI shows that you're <b>" + classification + "</b>";
+    return "Dear "+name+", Your BMI is: <b>"+ bmi +weightUnit+"/"+heightUnit+"<sup>2</sup></b> " + "<br/>Your BMI shows that you're <b><span style=\"color:" + classification + "</span>.</b>";
 }
 //a function that changes units based on user selection
 function changeUnits(selectedUnit, labelId, inputName){
@@ -68,14 +58,52 @@ function changeUnits(selectedUnit, labelId, inputName){
     document.getElementsByName(inputName)[0].placeholder= selectedUnit;
     return selectedUnit;
 }
-
 function validateName(name){
-     // Regular expression to match any alphabetical letter, capital or small, then (space or ' or -) and so on...making sure it doesn't end with
+        //remove extra empty spaces between the words (\s finds whitespace characters) and replcae replaces them with nothing. '/g/ finds all that match
+        name = name.replace(/\s+/g, '');
+        // Regular expression to match any alphabetical letter, capital or small, then (space or ' or -) and so on...making sure it doesn't end with
          //anything other than the specified characters using $, reference https://www.w3schools.com/jsref/jsref_obj_regexp.asp
-         var regex = /^[a-zA-Zا-ي]+(?:[' -][a-zA-Zا-ي]+)*$/;
+         var regex = /^[a-zA-Zا-ي]+(?:[ ][a-zA-Zا-ي]+)*$/;
          if(!regex.test(name) || name.length < 2 || name.length > 100){
              return true;
          }else{
             return false;
          }
+}
+function resetMessages(){
+    const messageFields = ["nameMessage","heightMessage","weightMessage","messageP", "decimalMessage"]
+    //reset all message fields.
+    messageFields.forEach(function(field){
+    passMessageToElement(field, "");
+    document.getElementById(field).style.display = "none";
+    });
+}
+
+function toggleMessagefield(messageField) {
+    var field = document.getElementById(messageField);
+    if (field.innerHTML.trim() !== "") {
+        field.style.display = "block";
+    } else {
+        field.style.display = "none";
+    }
+}
+
+function formatNumberWithDecimals(number, decimals) {
+    //convert the number to string then split the number where there's a decimal point.
+    var parts = number.toString().split(".");
+    //integer part is the part that's before the decimal point.
+    var integerPart = parts[0];
+    //after the decimal point, second element of the array.
+    var fractionalPart = parts[1] || "";
+    //add a 0 for every missing decimal.
+    while (fractionalPart.length < decimals) {
+        fractionalPart += "0";
+    }
+    // if it's 0, return integer only, if it's not 0, return int + fractional
+    if(decimals != 0){
+    return integerPart + "." + fractionalPart;
+    }
+    else{
+        return integerPart;
+    }
 }
