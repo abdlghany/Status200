33d599262fd6d2f61e96c3831e9437166a527710
abdlghany <abdalghany@windowslive.com>
2024-08-11 14:34:26 +0800

    wip

--- Stage2/Javascript/ATM/databaseConnection.js
+++ Stage2/Javascript/ATM/databaseConnection.js
@@ -3,13 +3,15 @@ var mysql = require('mysql2');
 var con = mysql.createConnection({
   host: "localhost",
   user: "abdalghany",
-  password: "ohitsme"
+  password: "ohitsme",
+  database:""
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+  con.query("SELECT * FROM TABLENAME", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
 });
-
-              
-
