0febd9f7fe67050ce3584e86670b2d2db7f4b937
abdlghany <abdalghany@windowslive.com>
2024-10-19 13:52:47 +0800

    wip

--- Stage3/vite/Vite-React-Project/src/App.tsx
+++ Stage3/vite/Vite-React-Project/src/App.tsx
@@ -5,10 +5,10 @@ import RevisionListGroup from './components/List/RevisionListGroup'
 const myList = ["Item 1", "Item 2", "Item 30", "Item 40", "..."];
 function App() {
   return (
-    <>
+    <div className='m-4s'>
       <Page/>
-      <RevisionListGroup name="My List" ListItems={myList}/>
-    </>
+        <RevisionListGroup name="My List" ListItems={myList}/>
+    </div>
   )
 }
 
--- Stage3/vite/Vite-React-Project/src/components/List/RevisionListGroup.tsx
+++ Stage3/vite/Vite-React-Project/src/components/List/RevisionListGroup.tsx
@@ -7,13 +7,13 @@ interface properties {
 export default function RevisionListGroup({name, ListItems}:properties){
 
     return(
-        <>
+        <div className="w-25 m-auto">
             <h2>{name}</h2>
             <ul className="list-group">
                 {ListItems?.map(
                     (element) => <RevisionListItem value={element}/>
                 )}
             </ul>
-        </>
+        </div>
     )
 }
